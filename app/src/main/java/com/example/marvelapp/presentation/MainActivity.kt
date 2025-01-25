1fcf73027aec228cc1f37d9f2caa53631c3c07e8
Ingrid Silva <ingridsilvatec@outlook.com>
2025-01-24 21:36:27 -0300

    add adiciona novo toolbar

--- app/src/main/java/com/example/marvelapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/marvelapp/presentation/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.navigation.ui.setupWithNavController
 import com.example.marvelapp.R
 import com.example.marvelapp.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
@@ -24,8 +25,19 @@ class MainActivity : AppCompatActivity() {
         navController = navHostFragment.navController
         binding.bottomNavMain.setupWithNavController(navController)
         appBarConfiguration = AppBarConfiguration(
-            setOf()
+            setOf(
+                R.id.charactersFragment, R.id.favoritesFragment, R.id.aboutFragment
+            )
         )
 
+        binding.toolbarApp.setupWithNavController(navController, appBarConfiguration)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            val isTopLevelDestination =
+                appBarConfiguration.topLevelDestinations.contains(destination.id)
+            if (!isTopLevelDestination) {
+                binding.toolbarApp.setNavigationIcon(R.drawable.ic_back)
+            }
+        }
     }
 }
